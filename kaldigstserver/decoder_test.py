abf02d1a2d9b9c99a98399d877bc9b263f9148f2
Tanel Alumae <alumae@gmail.com>
2013-09-27 12:58:17 +0300

    tests

---
+++ kaldigstserver/decoder_test.py
@@ -0,0 +1,106 @@
+'''
+Created on Jun 27, 2013
+
+@author: tanel
+'''
+import unittest
+from gi.repository import GObject, Gst
+import thread
+import logging
+from decoder import DecoderPipeline
+import time
+
+class DecoderPipelineTests(unittest.TestCase):
+
+    def testDecoder(self):
+        finished = [False]
+        loop = GObject.MainLoop()
+        thread.start_new_thread(loop.run, ())
+                
+        logging.basicConfig(level=logging.INFO)
+        decoder_conf = {"model" : "test/models/estonian/tri2b_mmi_pruned/final.mdl",
+                        "lda-mat" : "test/models/estonian/tri2b_mmi_pruned/final.mat",
+                        "word-syms" : "test/models/estonian/tri2b_mmi_pruned/words.txt",
+                        "fst" : "test/models/estonian/tri2b_mmi_pruned/HCLG.fst",
+                        "silence-phones" : "6"}
+        
+        
+        decoder_pipeline = DecoderPipeline({"decoder" : decoder_conf})
+        
+        words = []
+        
+        def word_getter(word):
+            words.append(word)
+            
+        def set_finished(finished):
+            finished[0] = True
+        
+        decoder_pipeline.set_word_handler(word_getter)
+        decoder_pipeline.set_eos_handler(set_finished, finished)
+        
+        def do_shit():
+            decoder_pipeline.init_request("test0", "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1")
+            f = open("test/data/lause2.raw", "rb")
+            for block in iter(lambda: f.read(2*16000), ""):
+                time.sleep(1)
+                decoder_pipeline.process_data(block)
+            
+            decoder_pipeline.end_request()
+    
+        do_shit()
+    
+        while not finished[0]:
+            time.sleep(1)
+        self.assertItemsEqual(["see", "on", "teine", "lause", "<#s>"], words, "Recognition result")
+        
+        words = []
+        
+        finished[0] = False    
+        do_shit()
+        while not finished[0]:
+            time.sleep(1)
+            
+        self.assertItemsEqual(["see", "on", "teine", "lause", "<#s>"], words, "Recognition result")
+        
+        # Now test cancelation
+        words = []        
+        decoder_pipeline.init_request("test0", "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1")
+        f = open("test/data/lause2.raw", "rb")
+        decoder_pipeline.process_data(f.read(2*16000))
+        decoder_pipeline.cancel()
+        print "Pipeline cancelled"
+        
+        words = []
+        finished[0] = False
+        decoder_pipeline.init_request("test0", "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1")
+        # read and send everything
+        f = open("test/data/lause2.raw", "rb")
+        decoder_pipeline.process_data(f.read(10*16000))
+        decoder_pipeline.end_request()
+        while not finished[0]:
+            time.sleep(1)            
+        self.assertItemsEqual(["see", "on", "teine", "lause", "<#s>"], words, "Recognition result")
+        
+        #test cancelling without anything sent
+        decoder_pipeline.init_request("test0", "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1")
+        decoder_pipeline.cancel()
+        print "Pipeline cancelled"
+        
+        words = []
+        finished[0] = False
+        decoder_pipeline.init_request("test0", "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1")
+        # read and send everything
+        f = open("test/data/lause2.raw", "rb")
+        decoder_pipeline.process_data(f.read(10*16000))
+        decoder_pipeline.end_request()
+        while not finished[0]:
+            time.sleep(1)            
+        self.assertItemsEqual(["see", "on", "teine", "lause", "<#s>"], words, "Recognition result")
+
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
---
+++ kaldigstserver/sample_client.py
@@ -0,0 +1,55 @@
+from ws4py.client.threadedclient import WebSocketClient
+import time
+import threading
+import sys
+import urllib
+import Queue
+
+class MyClient(WebSocketClient):
+    def opened(self):
+        fn = "test/data/test.raw"
+        if len(sys.argv) > 1:
+            fn = sys.argv[1]
+        print "Socket opened!"
+        def send_data_to_ws():
+            for i in range(1):
+                f = open(fn, "rb")
+                do_wait = False
+                for block in iter(lambda: f.read(2*16000), ""):
+                    if do_wait:
+                        time.sleep(1)
+                    ws.send(block, binary=True)
+                    do_wait = True
+            ws.send("EOS")
+        
+        t = threading.Thread(target=send_data_to_ws)
+        t.start()        
+
+    
+    def received_message(self, m):
+        print "WORD: %s" % m
+        
+    
+    def closed(self, code, reason=None):
+        print "Websocket closed() called"
+        # notify main thread
+        queue.put(1)
+        
+
+queue = Queue.Queue()
+
+content_type=""
+
+if len(sys.argv) > 1:
+    if sys.argv[1].endswith(".raw"):
+        content_type="audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1"
+        
+    
+ws = MyClient('ws://localhost:8888/speech?%s' % (urllib.urlencode([("content-type", content_type)])), protocols=['http-only', 'chat'])
+ws.connect()
+# wait until socket is closed
+MINUTE = 60
+queue.get(timeout=MINUTE)
+
+
+

791bffa5330517bf81bb4675b43db3ca186e5611
tanel <alumae@gmail.com>
2013-11-04 13:24:36 +0200

    experimenting with cutter

--- kaldigstserver/decoder.py
+++ kaldigstserver/decoder.py
@@ -49,8 +49,8 @@ class DecoderPipeline(object):
         self.appsrc.set_property("is-live", True)
         self.filesink.set_property("location", "/dev/null")
         self.cutter.set_property("leaky", False)
-        self.cutter.set_property("pre-length",   750 * 1000000)
-        self.cutter.set_property("run-length",   750 * 1000000)
+        self.cutter.set_property("pre-length",   1000 * 1000000)
+        self.cutter.set_property("run-length",   1000 * 1000000)
         #self.cutter.set_property("threshold", 0.05)
         logger.info('Created GStreamer elements')
 

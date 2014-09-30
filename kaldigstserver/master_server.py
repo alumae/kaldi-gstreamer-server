6ed6e59442da04ade7ef56a03d5f642a4dc22420
tanel <alumae@gmail.com>
2014-09-30 17:51:56 +0300

    Compability issues

--- kaldigstserver/master_server.py
+++ kaldigstserver/master_server.py
@@ -96,7 +96,9 @@ def content_type_to_caps(content_type):
         attributes = default_attributes
         for (key,_,value) in [p.partition("=") for p in attr_string.split(",")]:
             attributes[key.strip()] = value.strip()
-    return "%s, %s" % (media_type, ", ".join(["%s=%s" % (key, value) for (key,value) in attributes.iteritems()]))
+        return "%s, %s" % (media_type, ", ".join(["%s=%s" % (key, value) for (key,value) in attributes.iteritems()]))
+    else:
+        return content_type
 
 
 @tornado.web.stream_request_body

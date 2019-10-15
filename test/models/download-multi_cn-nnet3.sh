f9db0e9744c625342ba2001cda6e336e2a64c889
Xingyu Na <asr.naxingyu@gmail.com>
2019-10-15 17:16:26 +0900

    fix chinese model download

--- test/models/download-multi_cn-nnet3.sh
+++ test/models/download-multi_cn-nnet3.sh
@@ -3,7 +3,7 @@
 BASE_URL=http://kaldi-asr.org/models/11/
 MODEL=0011_multi_cn_chain_sp_online.tar.gz
 
-modeldir=`dirname $0`/chinese/multi_cn_chain_sp_online
+modeldir=`dirname $0`/chinese
 
 mkdir -p $modeldir
 
@@ -12,5 +12,8 @@ cd $modeldir
 wget -N $BASE_URL/$MODEL || exit 1
 tar -zxvf $MODEL
 rm $MODEL
+
+sed -i 's/=.*\/conf/=test\/models\/chinese\/multi_cn_chain_sp_online\/conf/g' multi_cn_chain_sp_online/conf/online.conf
+sed -i 's/=.*\/conf/=test\/models\/chinese\/multi_cn_chain_sp_online\/conf/g' multi_cn_chain_sp_online/conf/ivector_extractor.conf
 
 cd -
